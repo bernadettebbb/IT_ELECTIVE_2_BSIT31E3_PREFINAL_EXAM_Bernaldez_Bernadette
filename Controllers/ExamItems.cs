5bc10dd602f27306e05ee157829e58c9cee671d9
bernadettebbb <bernadettebernaldez02@gmail.com>
2026-09-12 02:34:52 +0800

    Update ExamItems.cs

--- Controllers/ExamItems.cs
+++ Controllers/ExamItems.cs
@@ -10,6 +10,7 @@ namespace PREFINAL_EXAM.Controllers
             var questions = new List<ExamItem>
             {
                 new ExamItem { Id = 1, Question = "What is the main problem solved by using a database instead of an in-memory collection?", Answer = "C. It allows data to persist after the application stops" },
+                new ExamItem { Id = 2, Question = "Which approach is being used when an existing database is used to generate EF Core entity classes?", Answer = "B. Database-First" },
             };
 
             return View(questions);
