37a8116656f7ba413d07b9a0d544ecea54db8d13
bernadettebbb <bernadettebernaldez02@gmail.com>
2026-09-12 02:33:35 +0800

    1

---
+++ Controllers/ExamItems.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PREFINAL_EXAM.Models;
+
+namespace PREFINAL_EXAM.Controllers
+{
+    public class ExamController : Controller
+    {
+        public IActionResult Index()
+        {
+            var questions = new List<ExamItem>
+            {
+                new ExamItem { Id = 1, Question = "What is the main problem solved by using a database instead of an in-memory collection?", Answer = "C. It allows data to persist after the application stops" },
+            };
+
+            return View(questions);
+        }
+    }
+}
---
+++ Models/ExamItem.cs
@@ -0,0 +1,9 @@
+﻿namespace PREFINAL_EXAM.Models
+{
+    public class ExamItem
+    {
+        public int Id { get; set; }
+        public string Question { get; set; } = string.Empty;
+        public string Answer { get; set; } = string.Empty;
+    }
+}
