b4af0ebfb314be9ca496f109a1e76c5893582917
bernadettebbb <bernadettebernaldez02@gmail.com>
2026-09-12 02:06:44 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IT_ELECTIVE_2_BSIT31E3_PREFINAL_EXAM_Bernaldez_Bernadette.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
