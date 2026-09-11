1ad445b2b6e3ed6ffff8e2a993f04d65a2d05916
bernadettebbb <bernadettebernaldez02@gmail.com>
2026-09-12 02:46:44 +0800

    Update ExamItems.cs

--- Controllers/ExamItems.cs
+++ Controllers/ExamItems.cs
@@ -15,7 +15,7 @@ namespace PREFINAL_EXAM.Controllers
                 new ExamItem { Id = 4, Question = "Which EF Core component is primarily responsible for communicating with the database?", Answer = "A. DbContext" },
                 new ExamItem { Id = 5, Question = "What does the command 'dotnet ef dbcontext scaffold' primarily do?", Answer = "C. Generates EF Core models and a DbContext from an existing database" },
                 new ExamItem { Id = 6, Question = "Where is a database connection string commonly stored in an ASP.NET Core MVC application?", Anr swe= "B. appsettings.json" },
-
+                new ExamItem { Id = 7, Question = "A Student belongs to exactly one Section, while a Section can contain many students. What type of relationship is this?", Answer = "B. One-to-Many" },
 
             };
 
