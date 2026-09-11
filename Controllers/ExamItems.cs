0a61afa4bf3ea6875b36f6b90e0f4b36648d9075
bernadettebbb <bernadettebernaldez02@gmail.com>
2026-09-12 02:52:26 +0800

    Update ExamItems.cs

--- Controllers/ExamItems.cs
+++ Controllers/ExamItems.cs
@@ -23,6 +23,7 @@ namespace PREFINAL_EXAM.Controllers
                 new ExamItem { Id = 12, Question = "What is the main benefit of Include(s => s.Section)?", Answer = "A. It loads the related Section navigation property" },
                 new ExamItem { Id = 13, Question = "Which type of validation occurs in the browser before a request is sent to the server?", Answer = "B. Client-side validation" },
                 new ExamItem { Id = 14, Question = "Why is server-side validation still necessary if client-side validation exists?", Answer = "A. Client-side validation can be bypassed" },
+                new ExamItem { Id = 15, Question = "A school requires every student to have a unique Student Number. Which rule best represents this requirement?", Answer = "B. Student Number should be unique" },
 
 
             };
