2adc6856b31747caa039dc57402061c9c4802aca
bernadettebbb <bernadettebernaldez02@gmail.com>
2026-09-12 02:55:38 +0800

    Update ExamItems.cs

--- Controllers/ExamItems.cs
+++ Controllers/ExamItems.cs
@@ -28,8 +28,7 @@ namespace PREFINAL_EXAM.Controllers
                 new ExamItem { Id = 17, Question = "What is the purpose of a try...catch block in a controller?", Answer = "B. To catch and handle exceptions that may occur during execution" },
                 new ExamItem { Id = 18, Question = "Which middleware is commonly used in ASP.NET Core for centralized exception handling?", Answer = "B. UseExceptionHandler()" },
                 new ExamItem { Id = 19, Question = "A user requests /Student/999, but Student 999 does not exist. What would be the most appropriate response?", Answer = "B. Display a Not Found (404) response/page" },
-
-
+                new ExamItem { Id = 20, Question = "A student already belongs to Section A for a particular subject. The application attempts to assign the same student to Section A again. What is the primary concern?", Answer = "A. Data integrity" }
             };
 
             return View(questions);
